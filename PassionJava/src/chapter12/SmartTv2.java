df12251da84e80ac097d4f25ba26427580829dd3
devgiants75 <devgiants75@naver.com>
2024-03-20 11:50:01 +0900

    다형성 1

---
+++ PassionJava/src/chapter12/SmartTv2.java
@@ -0,0 +1,23 @@
+package chapter12;
+
+public class SmartTv2 extends Tv2 {
+	
+	public static void main(String[] args) {
+//		Tv2 tv2=new Tv();
+//		int num=0.0;
+		Tv2 tv2=new Tv2();
+		SmartTv2 smartTv2=new SmartTv2();
+		//인스턴스 타입과 참조 변수의 타입이 일치 되어야 한다
+		Tv2 sTv2=new SmartTv2();
+//		Tv2 sTv21=new SmartTv();
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ PassionJava/src/chapter12/Tv2.java
@@ -0,0 +1,11 @@
+package chapter12;
+
+//다형성(polymorphism)
+public class Tv2 {
+	private boolean power;
+	
+	public void power() {
+		power=!power;
+	}
+
+}
